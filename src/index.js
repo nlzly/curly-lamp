57f2ac9eacdd92c7b8e6526da186375ae493aab7
Nick Leslie <nick.leslie@centare.com>
2018-07-18 11:14:28 -0500

    Added a counter and a click function

--- src/index.js
+++ src/index.js
@@ -7,28 +7,36 @@ var config = {
     height: 800,
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
 
+var score = 0;
+var scoreText;
+
 function preload ()
 {
     this.load.image('logo', 'assets/logo.png');
+    this.load.image('background', 'assets/blank_background.png');
 }
 
 function create ()
 {
-    var logo = this.add.image(400, 150, 'logo');
+    this.add.image(400, 400, 'background');
+    var logo = this.add.image(400, 400, 'logo').setInteractive();
+    logo.on('pointerup', incrementButtonClick);
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+}
 
+function update () {
+    scoreText.setText('Score: ' + score);
+}
+
+function incrementButtonClick() {
+    score = score+1;
+    console.log("button pressed");
 }
